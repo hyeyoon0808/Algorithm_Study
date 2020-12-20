4fa2a49ce404e8f667d76201bb73c755c8617cbc
ymh254 <ymh254@gmail.com>
2020-12-20 23:22:04 +0900

    add ymh254 Algorithm -first week

---
+++ BOJ/별찍기_2438/ymh254_star.py
@@ -0,0 +1,3 @@
+a = int(input())
+for i in range(0, a):
+    print("*"*(i+1))
---
+++ BOJ/세수_10817/ymh254_three.py
@@ -0,0 +1,6 @@
+a, b, c = map(int, input().split(" "))
+number = [a, b, c]
+number.sort()
+print(number)
+# number = sorted(number)
+print(number[1])

b5ec25935d1da78cc9394b414904a5ef540cbb7a
hyeyoon0808 <hyeyooncho08@gmail.com>
2020-12-21 21:04:28 +0900

    ADD comments

--- BOJ/세수_10817/ymh254_three.py
+++ BOJ/세수_10817/ymh254_three.py
@@ -1,6 +1,6 @@
 a, b, c = map(int, input().split(" "))
 number = [a, b, c]
-number.sort()
+number.sort(reverse=True)
 print(number)
 # number = sorted(number)
 print(number[1])
--- PRG/모의고사_42840/hyeyoon0808.py
+++ PRG/모의고사_42840/hyeyoon0808.py
@@ -26,13 +26,14 @@ countABC = [countA, countB, countC]
 # countABC 리스트 최댓값과 같은 사람 리스트에 저장
 # list 는 배열을 인덱스로 접근해야 함. 
 # enumerate 는 지연 제너레이터(lazy generator)로 이터레이터 감싸서 이터레이터에서 루프 인덱스와 다음 값을 한 쌍으로 가져와 넘김.
-for person, count in enumerate(countABC):                                 
+for person, count in enumerate(countABC):    
+    print(count)                             
     if count == max(countABC):
         answer.append(person +1)
 
-# 정답자 출력
-for i, p in enumerate(answer):
-    print(p)
+# # 정답자 출력
+# for i, p in enumerate(answer):
+#     print(p)
         
 
 
@@ -44,12 +45,17 @@ answers = list(map(int, input().split()))
 # 2차원 리스트 활용
 p = [[1,2,3,4,5],
     [2,1,2,3,2,4,2,5],
-    [3,3,1,1,2,2,4,4,5,5]]
+    [3,3,1,1,2,2,4,4,5,5]] # index=i : 0~3, 값=v: 3개의 list
 # p의 길이만큼 늘리기
 s = [0] * len(p)
+# s i=0 => 첫번째 리스트
+# s i=1 => 2번째 리스트
+# s i=2 => 3번째 리스트
+
+# answers = 1345 index = q: 0~3, 값 = a: 1,3,4,5
 
 # 이중 for loop
-for q, a in enumerate(answers):
+for q, a in enumerate(answers): # q =0 a=1
     for i, v in enumerate(p): # v는 각각 리스트의 길이인 5,6,7
         if a == v[q % len(v)]: # 나머지 값과 비교
             s[i] += 1 
